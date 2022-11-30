44b40c63aa2edc97381ba910b70bece10a356c4f
eid_gabriele <eid.gabriele@gmail.com>
2022-11-30 09:46:38 +0200

    role, person models

--- movie_database/movie_app/admin.py
+++ movie_database/movie_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'country',)
+
+
+admin.site.register(models.Person)
+admin.site.register(models.Role)
--- movie_database/movie_app/models.py
+++ movie_database/movie_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Role(models.Model):
+    name = models.CharField(_('name'), max_length=150, help_text=_('Enter the name of the role'))
+
+    def __str__(self)->str:
+        return self.name
+
+class Person(models.Model):
+    first_name = models.CharField(_('first name'), max_length=100)
+    last_name = models.CharField(_('last name'), max_length=100)
+    country = models.CharField(_('country'), max_length=100)
+    photo = models.ImageField(_('photo'), upload_to='cast_photos', blank=True, null=True)
+
+    def __str__(self)->str:
+        return f"{self.first_name} {self.last_name}"
