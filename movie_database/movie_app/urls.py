c218c2524017f8f36f0cd32e57bc34c517914b59
eid_gabriele <eid.gabriele@gmail.com>
2022-12-02 15:24:34 +0200

    views, templates for movies, movie

--- movie_database/movie_app/urls.py
+++ movie_database/movie_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home_page'),
-    path('movies/', views.movies, name='movies'),
+    path('movies/', views.MediaListView.as_view(), name='media_list'),
+    path('movies/<int:media_id>/', views.media, name='media'),
 ]
--- movie_database/movie_app/views.py
+++ movie_database/movie_app/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.contrib import messages
+from . models import Media, Person
 
 # Create your views here.
 
 def index(request):
     return render(request,'movie_app/home_page.html')
 
-def movies(request):
-    return render(request, 'movie_app/movies.html')
+def media(request, media_id):
+    return render(request, 'movie_app/media.html', {'media': get_object_or_404(Media, id=media_id)})
+
+class MediaListView(ListView):
+    model = Media
+    template_name = 'movie_app/movies.html'
+
+
