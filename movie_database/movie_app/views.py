1e7a9c1dcfb223a813c1e884e56fffab5f5fca65
eid_gabriele <eid.gabriele@gmail.com>
2022-12-06 15:38:24 +0200

    search media by media name, people first and last names

--- movie_database/movie_app/views.py
+++ movie_database/movie_app/views.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 from django.contrib import messages
 from . models import Media, Person, Company, Genre
 from django.views.generic.edit import FormMixin
+from django.db.models import Q
 
-# Create your views here.
 
 def index(request):
     return render(request,'movie_app/home_page.html')
@@ -13,12 +13,24 @@ def index(request):
 def media(request, media_id):
     return render(request, 'movie_app/media.html', {'media': get_object_or_404(Media, id=media_id)})
 
+def company(request, company_id):
+    return render(request, 'movie_app/company.html', {'company': get_object_or_404(Company, id=company_id)})
+
+def person(request, person_id):
+    return render(request, 'movie_app/person.html', {'person': get_object_or_404(Person, id=person_id)})
+
 class MediaListView(ListView):
     model = Media
     template_name = 'movie_app/media_list.html'
 
     def get_queryset(self):
         queryset = super().get_queryset()
+        search = self.request.GET.get('search')
+        if search:
+            queryset = queryset.filter(
+            Q(name__icontains=search) | 
+            Q(cast_crew__person__first_name__icontains=search) |
+            Q(cast_crew__person__last_name__icontains=search))
         if self.request.GET.get("is_series") != None:
             queryset = queryset.filter(is_series=self.request.GET.get("is_series"))
         genre_id = self.request.GET.get('genre_id')
@@ -34,20 +46,5 @@ class MediaListView(ListView):
             context['genre'] = get_object_or_404(Genre, id=genre_id)
         return  context
 
-def company(request, company_id):
-    return render(request, 'movie_app/company.html', {'company': get_object_or_404(Company, id=company_id)})
 
-def person(request, person_id):
-    return render(request, 'movie_app/person.html', {'person': get_object_or_404(Person, id=person_id)})
-
-# class GenreListView(ListView):
-#     model = Media
-#     template_name = 'movie_app/by_genre.html'
-
-#     def get_queryset(self):
-#         queryset = super().get_queryset()
-#         genre_id = self.request.GET.get('genre_id')
-#         if genre_id:
-#             queryset = queryset.filter(genre__id=genre_id)
-#         return queryset
     
