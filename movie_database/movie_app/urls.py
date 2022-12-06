120109595d77172b7a4fc5ad43528d5c3b1ee0ea
eid_gabriele <eid.gabriele@gmail.com>
2022-12-06 11:06:23 +0200

    series and movies views combined to one

--- movie_database/movie_app/urls.py
+++ movie_database/movie_app/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home_page'),
-    path('movies/', views.MovieListView.as_view(), name='movie_list'),
-    path('movies/<int:media_id>/', views.media, name='media'),
-    path('series/', views.SeriesListView.as_view(), name='series_list'),
+    path('media_list/', views.MediaListView.as_view(), name='media_list'),
+    path('media_list/<int:media_id>/', views.media, name='media'),
     path('companies/<int:company_id>/', views.company, name='company'),
     path('people/<int:person_id>/', views.person, name='person'),
-    path('by_genre/', views.GenreListView.as_view(), name='genres'),
+    # path('by_genre/', views.MediaListView.as_view(), name='genres'),
 ]
--- movie_database/movie_app/views.py
+++ movie_database/movie_app/views.py
@@ -13,41 +13,19 @@ def index(request):
 def media(request, media_id):
     return render(request, 'movie_app/media.html', {'media': get_object_or_404(Media, id=media_id)})
 
-class MovieListView(ListView):
+class MediaListView(ListView):
     model = Media
     template_name = 'movie_app/media_list.html'
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        queryset = queryset.filter(is_series=False).order_by('release_date')
-        return queryset
-
-class SeriesListView(ListView):
-    model = Media
-    template_name = 'movie_app/media_list.html'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.filter(is_series=True).order_by('release_date')
-        return queryset
-
-def company(request, company_id):
-    return render(request, 'movie_app/company.html', {'company': get_object_or_404(Company, id=company_id)})
-
-def person(request, person_id):
-    return render(request, 'movie_app/person.html', {'person': get_object_or_404(Person, id=person_id)})
-
-class GenreListView(ListView):
-    model = Media
-    template_name = 'movie_app/by_genre.html'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
+        if self.request.GET.get("is_series") != None:
+            queryset = queryset.filter(is_series=self.request.GET.get("is_series"))
         genre_id = self.request.GET.get('genre_id')
         if genre_id:
             queryset = queryset.filter(genre__id=genre_id)
-        return queryset
-    
+        return queryset.order_by('release_date')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         genre_id = self.request.GET.get('genre_id')
@@ -55,3 +33,21 @@ class GenreListView(ListView):
         if genre_id:
             context['genre'] = get_object_or_404(Genre, id=genre_id)
         return  context
+
+def company(request, company_id):
+    return render(request, 'movie_app/company.html', {'company': get_object_or_404(Company, id=company_id)})
+
+def person(request, person_id):
+    return render(request, 'movie_app/person.html', {'person': get_object_or_404(Person, id=person_id)})
+
+# class GenreListView(ListView):
+#     model = Media
+#     template_name = 'movie_app/by_genre.html'
+
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         genre_id = self.request.GET.get('genre_id')
+#         if genre_id:
+#             queryset = queryset.filter(genre__id=genre_id)
+#         return queryset
+    
